8a5f3ba8707397eb59e6b92b05bfc07630e8e8f0
Diurno <Diurno@C06PC25.informatica.comercio>
2023-11-30 11:52:03 +0100

    esto es una prueba

--- src/proyectoaad/ProyectoAAD.java
+++ src/proyectoaad/ProyectoAAD.java
@@ -14,6 +14,6 @@ public class ProyectoAAD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic herefdsafasddf
+        // TODO code application logic herefdsafasddffdasfsadfsdaf
     }
 }
